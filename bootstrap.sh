e3a91ea2b6a5dcda2d771c7d2ce9aa044e38c752
Ryan Beiden <ryanbeiden@pm.me>
2025-11-11 22:11:15 -0600

    Add shell for making env

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat <<EOF > .env
+
+APP_ENV=dev
+APP_SECRET
+DEFAULT_URI=http://localhost
+TOKEN=${TOKEN}
+BASE_URI=${BASE_URI}
+DISCORD_TOKEN=${DISCORD_TOKEN}
+
+EOF
+
+echo ".env file created!"
+
